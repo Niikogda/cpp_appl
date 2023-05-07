5102b6bc88fa7bace91832f2835f9e2a88323768
Laptop <Laptop@DESKTOP-BVI07RL>
2023-05-07 21:21:25 +0300

    done

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Name:\t\t'The war and the peace'\t" << endl;
+	cout << "Avtor: \t\tL.N. Tolstoj  " << endl;
+	cout << "Izdatelstvo:\tPiter " << endl;
+	cout << "Pages:\t\t500. " << endl;
+	return(0);
+}
+
