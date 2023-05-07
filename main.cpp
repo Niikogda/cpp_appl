8f2ee58783c99e24f09430d201d0ccb668cdd6a7
Laptop <Laptop@DESKTOP-BVI07RL>
2023-05-07 21:58:22 +0300

    done_not

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>;
+using namespace std;
+
+int main()
+
+{
+    cout << "\n" << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 << endl;
+    cout << (char)186 << " Pory Roku \t\t" << (char)32 << (char)32 << (char)32 << (char)32 << (char)32 << (char)186 << endl;
+    cout << (char)204 << (char)219 << (char)205 << (char)205 << (char)205  << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205  << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)185 << endl;
+    cout << (char)186 << " Zyma " << (char)186 << " Vesna " << (char)186 << " Lito " << (char)186 << " Osin " << (char)186 << endl;
+    cout << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)188 << endl;
+
+    return 0;
+}
+
+
+
