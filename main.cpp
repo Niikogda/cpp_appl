aa3668887aee8ff19477ade007fc67db3f2475ca
Laptop <Laptop@DESKTOP-BVI07RL>
2023-05-07 21:31:29 +0300

    done

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "'Kraj lukomor’a dub zelenyj,\n\aI zolotyj lancyuh na nim :\n\aShodnya, shonochi kit uchenyj\n\aNa lancyuhu kruzhl’aje tim '" << endl;
+}
